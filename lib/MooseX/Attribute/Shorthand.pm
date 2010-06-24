c79f16aa7fdad589f8b29b0a9d4ae39b66f6efbe
Jesse Luehrs <doy@tozt.net>
2010-06-24 13:54:24 -0500

    convert to new dzil stuff

--- lib/MooseX/Attribute/Shorthand.pm
+++ lib/MooseX/Attribute/Shorthand.pm
@@ -1,13 +1,10 @@
 package MooseX::Attribute::Shorthand;
 use strict;
 use warnings;
+# ABSTRACT: write custom attribute option bundles
 
 use Moose ();
 use Scalar::Util qw(reftype);
-
-=head1 NAME
-
-MooseX::Attribute::Shorthand -
 
 =head1 SYNOPSIS
 
@@ -69,55 +66,9 @@ sub import {
     );
 }
 
-=head1 BUGS
-
-No known bugs.
-
-Please report any bugs through RT: email
-C<bug-moosex-attribute-shorthand at rt.cpan.org>, or browse to
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-Attribute-Shorthand>.
-
 =head1 SEE ALSO
 
-
-=head1 SUPPORT
-
-You can find this documentation for this module with the perldoc command.
-
-    perldoc MooseX::Attribute::Shorthand
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MooseX-Attribute-Shorthand>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MooseX-Attribute-Shorthand>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-Attribute-Shorthand>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MooseX-Attribute-Shorthand>
-
-=back
-
-=head1 AUTHOR
-
-  Jesse Luehrs <doy at tozt dot net>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2010 by Jesse Luehrs.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
+L<MooseX::Attributes::Curried>
 
 =cut
 
--- t/000-load.t
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use Test::More tests => 1;
-
-package Foo;
-use Moose;
-::use_ok('MooseX::Attribute::Shorthand')
-    or ::BAIL_OUT("couldn't load MooseX::Attribute::Shorthand");
